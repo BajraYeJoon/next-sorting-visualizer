2e35e405d335cc62c7a23f7bbea3f3208392bddd
bajrayejoon <binaya.bajrashakya@gmail.com>
2024-07-12 10:39:06 +0545

    chore: Add @radix-ui/react-slider dependency and implement Slider component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,42 @@
 "use client";
+import { Slider } from "@/components/ui/slider";
 import { useSortingAlgorithmContext } from "@/context/visualizer";
-import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
-  const { arrayToSort } = useSortingAlgorithmContext();
+  const { arrayToSort, animationSpeed, isSorting, setAnimationSpeed } =
+    useSortingAlgorithmContext();
+
+  useEffect(() => {
+    console.log("animationSpeed", animationSpeed);
+  }, [animationSpeed]);
 
   return (
     <main className="absolute top-0 h-screen w-screen z-[-2] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#150229_1px)] bg-[size:40px_40px]">
       <div className="flex h-full justify-center">
         <div
           id="content-container"
-          className="flex max-w-[1280px] w-full flex-col lg:px-0 px-4"
+          className="flex max-w-[1020px] w-full flex-col lg:px-0 px-4"
         >
           <div className="h-[66px] relative flex items-center justify-between w-full">
             <h1 className="text-gray-300 text-2xl font-light hidden md:flex">
-              Sorting Visualizer
+              Sorting Visulizer
             </h1>
-            <div className="flex items-center justify-center gap-4">
-              Controls
+            <div className="flex items-center justify-center max-w-[1280px] gap-4 bg-white">
+              <Slider
+                disabled={isSorting}
+                value={[animationSpeed]}
+                handleChange={(e) => setAnimationSpeed(Number(e.target.value))}
+              />
             </div>
           </div>
-
           <div className="relative h-[calc(100vh-66px)] w-full">
             <div className="absolute bottom-[32px] w-full mx-auto left-0 right-0 flex justify-center items-end">
               {arrayToSort.map((value, index) => (
                 <div
                   key={index}
-                  className="array-line relative w-1 mx-0.5 shadow-lg opacity-70 rounded-lg bg-purple-500"
-                  style={{
-                    height: `${value}px`,
-                  }}
+                  className="array-line relative w-1 mx-0.5 shadow-lg opacity-70 rounded-lg bg-purple-400"
+                  style={{ height: `${value}px` }}
                 ></div>
               ))}
             </div>
